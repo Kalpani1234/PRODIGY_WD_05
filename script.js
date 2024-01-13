a1e5d99103ed3fd7e6a990c1c8a101e126ef81a9
Kalpani1234 <Kalpani4lk@gmail.com>
2024-01-13 11:37:02 +0530

    doing changes

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function getWeather() {
     const apiKey = '593d302904bdccd091faa41e318bc378';
-    const city = "colombo";
+    const city = document.getElementById("city").value;
 
     if (!city) {
         alert('Please enter your city');
         return;
     }
 
-    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
-
-    fetch(currentWeatherUrl)
-        .then(response => response.json())
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
             displayWeather(data);
         })
@@ -20,14 +22,19 @@ function getWeather() {
             alert('Error fetching current weather data. Please try again.');
         });
 
-    fetch(forecastUrl)
-        .then(response => response.json())
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
             displayHourlyForecast(data.list);
         })
         .catch(error => {
             console.error('Error fetching hourly forecast data:', error);
-            alert('Error fetching hourly forecast data. Please try again.');
+            alert('Error fetching current weather data. Please try again.');
         });
 }
 
